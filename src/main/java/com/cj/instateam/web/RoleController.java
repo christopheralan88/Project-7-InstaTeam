8e4778267953c2e91d01488952c07aaceeac9d27
christopheralan88 <christopheralan88@gmail.com>
2017-03-23 22:57:42 -0400

    working on RoleController

--- src/main/java/com/cj/instateam/web/RoleController.java
+++ src/main/java/com/cj/instateam/web/RoleController.java
@@ -5,6 +5,7 @@ import com.cj.instateam.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,10 +26,26 @@ public class RoleController {
         return "roles";
     }
 
-    @RequestMapping(value = "/roles", method = RequestMethod.POST)
+    @RequestMapping(value = "/roles", method = RequestMethod.POST, params = {"role_name"})
     public String addRole(@RequestParam(value = "role_name") String name) {
         Role role = new Role().setName(name);
         roleService.save(role);
         return "redirect:/roles";
+    }
+
+    @RequestMapping(value = "/roles", method = RequestMethod.POST, params = {"role_id", "role_name"})
+    public String editRole(@RequestParam(value = "role_id") String id,
+                           @RequestParam(value = "role_name") String name) {
+        Role role = new Role().setId(Integer.parseInt(id))
+                              .setName(name);
+        roleService.save(role);
+        return "redirect:/roles";
+    }
+
+    @RequestMapping(value = "/edit_role/{id}", method = RequestMethod.GET)
+    public String viewEditRole(@PathVariable int id, ModelMap model) {
+        Role role = roleService.findById(id);
+        model.put("role", role);
+        return "edit_role";
     }
 }
