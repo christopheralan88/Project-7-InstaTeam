db99b1570148a11b11a382672c0e3f75b4d1c272
christopheralan88 <christopheralan88@gmail.com>
2017-04-05 00:16:44 -0400

    got validation to work for RoleController

--- src/main/java/com/cj/instateam/web/RoleController.java
+++ src/main/java/com/cj/instateam/web/RoleController.java
@@ -1,15 +1,19 @@
 package com.cj.instateam.web;
 
+import com.cj.instateam.model.Collaborator;
 import com.cj.instateam.model.Role;
 import com.cj.instateam.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 
@@ -22,26 +26,30 @@ public class RoleController {
     public String viewRoles(ModelMap model) {
         List<Role> roles = roleService.findAll();
         roles.stream().sorted((a, b) -> - a.getName().compareTo(b.getName()));
-        model.put("role", new Role());
+        model.put("newRole", new Role());
         model.put("roles", roles);
         return "roles";
     }
 
-    @RequestMapping(value = "/roles", method = RequestMethod.POST, params = {"role_name"})
-    public String addRole(@RequestParam(value = "role_name") String name) {
-        Role role = new Role().setName(name);
+    @RequestMapping(value = "/roles", method = RequestMethod.POST)
+    public String addRole(@Valid Role role, BindingResult result, RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            redirectAttributes.addFlashAttribute("role", role);
+            redirectAttributes.addFlashAttribute("errors", "Please fill out the form completely");
+            return "redirect:/errors";
+        }
         roleService.save(role);
         return "redirect:/roles";
     }
 
-    @RequestMapping(value = "/edit_role/{id}", method = RequestMethod.POST)
+    /*@RequestMapping(value = "/edit_role/{id}", method = RequestMethod.POST)
     public String editRole(@PathVariable String id,
                            @RequestParam(value = "role_name") String name) {
         Role role = new Role().setId(Integer.parseInt(id))
                               .setName(name);
         roleService.save(role);
         return "redirect:/roles";
-    }
+    }*/
 
     @RequestMapping(value = "/edit_role/{id}", method = RequestMethod.GET)
     public String viewEditRole(@PathVariable int id, ModelMap model) {
