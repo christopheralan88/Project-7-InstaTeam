6340c3dbfdba0306af8c21475d22e6b7fbed4951
christopheralan88 <christopheralan88@gmail.com>
2017-04-07 09:34:45 -0400

    tweaked CSS for error page

--- src/main/java/com/cj/instateam/web/ProjectController.java
+++ src/main/java/com/cj/instateam/web/ProjectController.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.HashMap;
